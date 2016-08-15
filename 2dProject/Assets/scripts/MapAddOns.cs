4c5f5895f124f0650ef4fc9d90138ff70b06a998
brobear988 <ultima988@gmail.com>
2016-08-15 05:28:20 -0700

    Doors wont spawn on low ceiling

--- 2dProject/Assets/scripts/MapAddOns.cs
+++ 2dProject/Assets/scripts/MapAddOns.cs
@@ -20,11 +20,11 @@ public class MapAddOns : MonoBehaviour
         }
 
         //Pass border size to script
-        for (int x = borderSize; x < map.GetLength(0) - borderSize; x++)
+        for (int x = 1; x < map.GetLength(0) - 1; x++)
         {
-            for (int y = borderSize; y < map.GetLength(1) - borderSize; y++)
+            for (int y = 0; y < map.GetLength(1); y++)
             {
-                if(map[x,y] == 0 && map[x + 1, y] == 0 && map[x - 1, y] == 0 && map[x, y + 1] == 0 && map[x, y - 1] == 1)
+                if (map[x, y] == 0 && map[x + 1, y] == 0 && map[x - 1, y] == 0 && map[x, y + 1] == 0 && map[x, y - 1] == 1 && map[x + 1, y - 1] == 1 && map[x - 1, y - 1] == 1 && map[x, y + 2] == 0)
                 {
                     //door
                     var doorTransform = Instantiate(doorPrefab) as Transform;
