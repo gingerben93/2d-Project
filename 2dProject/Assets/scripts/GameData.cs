ae1eb5e12a48fcaad16a0309decec7ddb4bf8e8d
brobear988 <ultima988@gmail.com>
2016-08-16 07:41:07 -0700

    Starting Script for Game Data

---
+++ 2dProject/Assets/scripts/GameData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameData : MonoBehaviour
+{
+
+    List<float> mapSeed = new List<float>();
+
+    public float getSeed(float seed, int doorNum)
+    {
+        float nextSeed;
+        if (doorNum == 1)
+        {
+            nextSeed = mapSeed.IndexOf(seed) + 1;
+        }
+        else
+        {
+            nextSeed = mapSeed.IndexOf(seed) - 1;
+        }
+
+        return nextSeed;
+    }
+
+    public void AddSeed(float seed)
+    {
+        mapSeed.Add(seed);
+    }
+
+}
