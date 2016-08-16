8e177c8adbc504e5548c05e8d3f50010c56cab31
brobear988 <ultima988@gmail.com>
2016-08-16 05:57:33 -0700

    Door Collision

---
+++ 2dProject/Assets/scripts/DoorCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorCollider : MonoBehaviour {
+
+    public void OnCollisionEnter2D(Collision2D node)
+    {
+        if (node.gameObject.tag == "Door")
+        {
+            Destroy(node.gameObject);
+        }
+    }
+}
