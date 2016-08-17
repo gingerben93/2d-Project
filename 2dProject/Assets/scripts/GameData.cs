b3575e3d7b64ec94ff1b8cad267f7c5e2ef1f816
gingerben93 <gingerben93@gmail.com>
2016-08-17 00:28:39 -0700

    Fix last commit
    read pref commit

--- 2dProject/Assets/scripts/GameData.cs
+++ 2dProject/Assets/scripts/GameData.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-<<<<<<< HEAD
-public class GameData : MonoBehaviour {
+public class GameData : MonoBehaviour
+{
 
     List<string> mapSeed = new List<string>();
 
@@ -22,45 +22,21 @@ public class GameData : MonoBehaviour {
     public string getSeed(string seed)
     {
         string nextSeed;
-        if(mapSeed.Count > 0) {
+        if (mapSeed.Count > 0)
+        {
             nextSeed = mapSeed[(mapSeed.IndexOf(seed) + 1) % mapSeed.Count];
         }
         else
         {
             return "0";
-=======
-public class GameData : MonoBehaviour
-{
-
-    List<float> mapSeed = new List<float>();
-
-    public float getSeed(float seed, int doorNum)
-    {
-        float nextSeed;
-        if (doorNum == 1)
-        {
-            nextSeed = mapSeed.IndexOf(seed) + 1;
-        }
-        else
-        {
-            nextSeed = mapSeed.IndexOf(seed) - 1;
->>>>>>> origin/master
         }
 
         return nextSeed;
     }
 
-<<<<<<< HEAD
     public void AddSeed(string seed)
-=======
-    public void AddSeed(float seed)
->>>>>>> origin/master
     {
         mapSeed.Add(seed);
     }
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> origin/master
