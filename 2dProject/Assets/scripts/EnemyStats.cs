6bce97b1e7db09d5db6ee06833fe379ea07d6dc3
brobear988 <ultima988@gmail.com>
2016-10-07 15:36:36 -0700

    Enemy script/health/shot damage + teemo marker

---
+++ 2dProject/Assets/scripts/EnemyStats.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// Handle hitpoints and damages
+public class EnemyStats : MonoBehaviour
+{
+    /// Total hitpoints
+    public int hp = 1;
+
+    public bool isEnemy = true;
+
+    /// Inflicts damage and check if the object should be destroyed
+    public void Damage(int damageCount)
+    {
+        hp -= damageCount;
+
+        if (hp <= 0)
+        {
+            // Dead!
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Is this a shot?
+        Shot shot = otherCollider.gameObject.GetComponent<Shot>();
+        if (shot != null)
+        {
+            // Avoid friendly fire
+            if (shot.isEnemyShot != isEnemy)
+            {
+                Damage(shot.damage);
+
+                // Destroy the shot
+                Destroy(shot.gameObject); // Remember to always target the game object, otherwise you will just remove the script
+            }
+        }
+    }
+}
--- 2dProject/Assets/scripts/Shot.cs
+++ 2dProject/Assets/scripts/Shot.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class Shot : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public int damage = 1;
+
+    /// Projectile damage player or enemies?
+    public bool isEnemyShot = false;
+
+    // Use this for initialization
+    void Start () {
         // 2 - Limited time to live to avoid any leak
         Destroy(gameObject, 2);
     }
